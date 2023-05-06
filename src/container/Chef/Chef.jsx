fecee71295af5b1e784cab92fc72da32f7ace2b7
BTran418 <btran418@gmail.com>
2023-05-06 04:12:39 -0700

    Completed Chef page & updated mobile views.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={ images.chef } alt="chef img"/>
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word"/>
+      <h1 className='headtext__cormorant'>What We Believe In</h1>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote" />
+          <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsa numquam nobis quasi dignissimos quos ullam ab iusto, iure modi porro aut illo rem dolor, itaque quam, sit aliquam voluptatum.</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef and Founder</p>
+        <img src={images.sign} alt="sign" />
+      </div>
+    </div>
   </div>
 );
 
